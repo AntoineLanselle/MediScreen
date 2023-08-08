43445ee37d03a78ad245dc9049563d41c6e75d0f
AntoineLanselle <antoine_lanselle@msn.com>
2023-08-08 15:46:13 +0200

    Clean package of all services
    - Small fixes have been applied to facilitate oral demonstration

--- PatientAssessmentService/src/main/java/com/mediscreen/PatientAssesmentService/proxies/NotesPatientServiceProxy.java
+++ PatientAssessmentService/src/main/java/com/mediscreen/PatientAssesmentService/proxies/NotesPatientServiceProxy.java
@@ -3,12 +3,7 @@ package com.mediscreen.PatientAssesmentService.proxies;
 import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.mediscreen.PatientAssesmentService.beans.NotesPatientBean;
@@ -23,19 +18,7 @@ import com.mediscreen.PatientAssesmentService.beans.NotesPatientBean;
 @FeignClient(name = "NotesPatientService", url = "${notes.patient.url}")
 public interface NotesPatientServiceProxy {
 
-	@GetMapping("/patHistory/{notesId}")
-	public NotesPatientBean getNotesById(@PathVariable("notesId") int notesId);
-
 	@GetMapping("/patHistory")
 	public List<NotesPatientBean> getAllPatientNotes(@RequestParam int patId);
-
-	@PostMapping(path = "/patHistory/add")
-	public String addNotesJson(@RequestBody NotesPatientBean notesPatientBean);
-
-	@PutMapping("/patHistory/update")
-	public String updateNotes(@RequestBody NotesPatientBean NotesPatientBean);
-
-	@DeleteMapping("/patHistory/delete/{notesId}")
-	public String deleteNotes(@PathVariable("notesId") int notesId);
 
 }
--- PatientAssessmentService/src/main/java/com/mediscreen/PatientAssesmentService/proxies/PatientApiServiceProxy.java
+++ PatientAssessmentService/src/main/java/com/mediscreen/PatientAssesmentService/proxies/PatientApiServiceProxy.java
@@ -3,12 +3,8 @@ package com.mediscreen.PatientAssesmentService.proxies;
 import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.mediscreen.PatientAssesmentService.beans.PatientBean;
@@ -23,9 +19,6 @@ import com.mediscreen.PatientAssesmentService.beans.PatientBean;
 @FeignClient(name = "PatientApiService", url = "${patient.api.url}")
 public interface PatientApiServiceProxy {
 
-	@GetMapping("/patient")
-	public List<PatientBean> getAllPatients();
-
 	@GetMapping("/patient/search")
 	public List<PatientBean> searchPatients(@RequestParam(required = false) String firstname,
 			@RequestParam(required = false) String lastname);
@@ -33,12 +26,4 @@ public interface PatientApiServiceProxy {
 	@GetMapping("/patient/{id}")
 	public PatientBean getPatient(@PathVariable("id") int patientId);
 
-	@PostMapping("/patient/add")
-	public String addPatientJson(@RequestBody PatientBean patientDto);
-
-	@PutMapping("/patient/update/{id}")
-	public String updatePatient(@PathVariable("id") int patientId, @RequestBody PatientBean patientBean);
-
-	@DeleteMapping("/patient/delete/{id}")
-	public String deletePatient(@PathVariable("id") int patientId);
 }
